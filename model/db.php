b8144bc73bf75f9aff65781adee0e8c5977a3550
chelnikovss <chelnikovss@gmail.com>
2016-04-14 10:48:48 +0300

    111

---
+++ model/db.php
@@ -0,0 +1,43 @@
+<?php
+//header('Content-Type: text/html; charset=utf-8');
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbname = "Routecalculation";
+
+$coordinate = array();
+
+@$conn = new mysqli($serverName, $userName, $password, $dbname);
+
+if(!$conn)
+{
+    die("Error !".mysqli_connect_error());
+}
+$conn->query("SET NAMES 'utf8'");
+
+$data = $_POST['officesPost'];
+
+$dataIdCity = explode('|', $data);
+
+$sql = "SELECT * FROM $dataIdCity[1] WHERE  id = $dataIdCity[0]";
+
+$res = $conn->query($sql);
+
+if($res->num_rows>0)
+{
+    while($row = $res->fetch_assoc())
+    {
+        $coordinate['latlocation'] = $row['latlocation'];
+        $coordinate['lnglocation'] = $row['lnglocation'];
+        $coordinate['indexmail'] = $row['indexmail'];
+
+        echo json_encode($coordinate);
+
+    }
+}
+
+$conn->close();
+
+
+
+
